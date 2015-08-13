463f75a8f3a2eaf634a1dbd9f0201ba2cb233f29
Domenico Rosario Caldesi <d.r.caldesi@gmail.com>
2015-08-13 18:20:22 +0200

    Modified TestLauncer, added output rendering

--- testEngine/TestLauncher.php
+++ testEngine/TestLauncher.php
@@ -6,6 +6,8 @@
 class TestLauncher {
 
     private $instance;
+    private $failures;
+    private $successes;
 
     public function __construct($instance) {
         if (!$instance)
@@ -14,6 +16,8 @@ class TestLauncher {
     }
 
     public function doTest() {
+        $this->failures = array();
+        $this->successes = array();
         $reflector = new ReflectionClass($this->instance);
         if (!$reflector->isSubclassOf('TestCase'))
             throw new Exception('Instance to test is not a TestCase subclass!');
@@ -21,7 +25,6 @@ class TestLauncher {
         $refreshForEachFuction = $reflector->getProperty('refreshForEachFuction')->getValue($this->instance);
         if (!$refreshForEachFuction)
             $reflector->getMethod('setUp')->invoke($this->instance);
-
         $methods = $reflector->getMethods();
         foreach ($methods as $method) {
             $methodName = $method->getName();
@@ -30,6 +33,7 @@ class TestLauncher {
                     $reflector->getMethod('setUp')->invoke($this->instance);
                 try {
                     $method->invoke($this->instance);
+                    $this->handleSuccess($methodName);
                 } catch (AssertionFailedException $afe) {
                     $this->handleAssertionFailed($methodName, $afe);
                 } catch (Exception $e) {
@@ -39,9 +43,64 @@ class TestLauncher {
                     $reflector->getMethod('cleanUp')->invoke($this->instance);
             }
         }
-
         if (!$refreshForEachFuction)
             $reflector->getMethod('cleanUp')->invoke($this->instance);
+
+        $this->renderResult($reflector->getName());
+    }
+
+    private function buildResult($className) {
+        $result = "";
+        $result .= "<!DOCTYPE html>";
+        $result .= "<html>";
+        $result .= "<head>";
+        $result .= "<link href=\"css/test-style.css\" rel=\"stylesheet\" type=\"text/css\">";
+        $result .= "</head>";
+        $result .= "<body>";
+        $result .= "<h1>";
+        $result .= "Test results for $className:";
+        $result .= "</h1>";
+
+        if (sizeof($this->failures) > 0) {
+            $result .= "<div class=\"failed\">";
+            $result .= "<h2>Failed:</h2>";
+            $result .= "<ul type=\"disc\">";
+            foreach ($this->failures as $method => $message) {
+                $result .= "<li>";
+                $result .= "<h3>";
+                $result .= $method . '()';
+                $result .= "</h3>";
+                $result .= $message;
+                $result .= "</li>";
+            }
+            $result .= "</ul>";
+            $result .= "</div>";
+        }
+
+        if (sizeof($this->successes) > 0) {
+            $result .= "<div class=\"passed\">";
+            $result .= "<h2>Passed:</h2>";
+            $result .= "<ul type=\"disc\">";
+            foreach ($this->successes as $method => $message) {
+                $result .= "<li>";
+                $result .= "<h3>";
+                $result .= $method . '()';
+                $result .= "</h3>";
+                $result .= $message;
+                $result .= "</li>";
+            }
+            $result .= "</ul>";
+            $result .= "</div>";
+        }
+
+        $result .= "</body>";
+        $result .= "</html>";
+
+        return $result;
+    }
+
+    private function renderResult($className) {
+        echo $this->buildResult($className);
     }
 
     private static function startsWith($haystack, $needle) {
@@ -49,14 +108,25 @@ class TestLauncher {
         return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== FALSE;
     }
 
+
+    // HANDLERS FOR TEST RESULTS
+
     private function handleAssertionFailed($methodName, AssertionFailedException $afe) {
-        // TODO
-        echo "Assertion Failed on $methodName: " . $afe->getMessage() . "<br/>";
+        $lineNumber = $afe->getTrace();
+        $lineNumber = $lineNumber[0]['line'];
+        $message = "Assertion Failed (@line " . $lineNumber . "): " . $afe->getMessage();
+        $this->failures[$methodName] = $message;
     }
 
     private function handleException($methodName, Exception $e) {
-        // TODO
-        echo "Exception on $methodName: " . $e->getMessage(). "<br/>";
+        $lineNumber = $e->getLine();
+        $message = "Exception (@line " . $lineNumber . "): " . $e->getMessage();
+        $this->failures[$methodName] = $message;
+    }
+
+    private function handleSuccess($methodName) {
+        $message = null;
+        $this->successes[$methodName] = $message;
     }
 
 }
